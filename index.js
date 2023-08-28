bc18195cc194394b4c6ea613b3ff07b9c26f8f7b
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-28 15:45:57 +0300

    Use showClock function

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import Book from './modules/book.js';
-import { showClock } from './modules/date-time.js';
+import showClock from './modules/date-time.js';
 
 const newBook = new Book();
 window.addEventListener('load', newBook.addBook);
+
+// Show clock initially
+showClock();
+
+// Update the clock and HTML element's content every second
+setInterval(showClock, 1000);
