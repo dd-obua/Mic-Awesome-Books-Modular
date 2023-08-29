12b9edc277ba36fcc3b56f2ed7434c4e538d0b13
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-29 13:09:31 +0300

    Import and use variable showDate

--- modules/date-time.js
+++ modules/date-time.js
@@ -1,11 +1,11 @@
 import { DateTime } from './luxon.js';
+import { showDate } from './select-elements.js';
 
 const showClock = () => {
   const now = DateTime.local();
   const formattedDate = now.toFormat("LLL d' 'yyyy, h:mm:ss a");
 
   // Update the HTML element's content with the formatted time
-  const showDate = document.querySelector('.show-date');
   showDate.textContent = formattedDate;
 };
 
